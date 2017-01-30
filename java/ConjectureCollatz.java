2105c73941ef911bbe1aa2729eb5a6845666aa34
Florian C <poslomc@gmx.fr>
2017-01-30 14:55:06 +0100

    Conjecture de Collatz
    Now show all calculated numbers.

--- java/ConjectureCollatz.java
+++ java/ConjectureCollatz.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Main{
+public class Main{
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     int start = -1;
@@ -13,11 +13,14 @@ class Main{
     int calc = start;
     int time = 0;
     
+    System.out.print(start);
     while(calc != 1){
       if(calc % 2 == 0) calc /= 2;
       else calc = 3*calc + 1;
       time++;
+      System.out.print(calc);
     }
+    System.out.println("");
     
     System.out.println("Found 1 as result by " + time + " calculations.");
   }
