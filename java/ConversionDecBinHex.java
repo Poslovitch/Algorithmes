e7a11b5b43dd8342cf99fad26eb89816d8428809
Florian C <poslomc@gmx.fr>
2017-01-31 13:37:08 +0100

    Conversion Décimal, Hexadécimal, Binaire - WIP

---
+++ java/ConversionDecBinHex.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Conversion{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        char continue, mode;
+        
+        do{
+            continue = ' ';
+            mode = ' ';
+            
+            // Mode
+            do{
+                System.out.println("Choix du mode de conversion:");
+                System.out.println("1: Décimal -> Binaire & Héxadécimal");
+                System.out.println("2: Binaire -> Décimal & Héxadécimal");
+                System.out.println("3: Hexadécimal -> Décimal & Binaire");
+                
+                mode = sc.nextLine().charAt(0);
+                if(mode != '1' && mode != '2' && mode != '3') System.out.println("Choix inconnu, veuillez réitérer.");
+            } while(mode != '1' && mode != '2' && mode != '3');
+        
+            String dec, bin, hex;
+            
+        } while(continue == 'O');
+        
+        sc.close();
+    }
+}
