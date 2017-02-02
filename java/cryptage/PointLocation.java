ccebc4341f0c4f30c110a724ca97b9ad66cc2b5e
Florian C <poslomc@gmx.fr>
2017-02-02 11:48:20 +0100

    PointLocation

---
+++ java/cryptage/PointLocation.java
@@ -0,0 +1,12 @@
+package java.cryptage;
+
+public class PointLocation{
+    
+    private double[] location;
+    
+    public PointLocation(double[] location){
+        this.location = location;
+    }
+    
+    public double[] getLocation(){return location;}
+}
