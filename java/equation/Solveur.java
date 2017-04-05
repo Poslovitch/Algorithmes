8e424e42f8e17d1664f279f2a5176023d7f9e6d5
Florian C <poslomc@gmx.fr>
2017-04-05 11:41:49 +0200

    Update Solveur.java

--- java/equation/Solveur.java
+++ java/equation/Solveur.java
@@ -1,3 +1,11 @@
-public class Solveur{
+package java.equation;
 
+import java.util.Scanner;
+
+public class Solveur{
+    public static class main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        sc.close();
+    }
 }
